d8a7aae509f992b1fc641adff09f49bb5796c95c
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-04-07 11:05:03 -1000

    completed exercise

--- Be-Positive/js/Be-Positive.js
+++ Be-Positive/js/Be-Positive.js
@@ -1,23 +1,20 @@
 BloodType = {
-
-  AB_POS : "AB_POS",
-  AB_NEG : "AB_NEG",
-  A_POS  : "A_POS",
-  A_NEG  : "A_NEG",
-  B_POS  : "B_POS",
-  B_NEG  : "B_NEG",
-  O_POS  : "O_POS",
-  O_NEG  : "O_NEG"
-
+  AB_POS: "AB_POS",
+  AB_NEG: "AB_NEG",
+  A_POS: "A_POS",
+  A_NEG: "A_NEG",
+  B_POS: "B_POS",
+  B_NEG: "B_NEG",
+  O_POS: "O_POS",
+  O_NEG: "O_NEG"
 };
 
 BloodTransfusionRules = {
-
   /**
    * Set the simulation speed.
    * @type {Number} : Valid values between 1 and 200
    */
-  simulation_speed : 90,
+  simulation_speed: 200,
 
   /**
    * returns BloodType, or false to give no BloodType
@@ -45,17 +42,113 @@ BloodTransfusionRules = {
    *
    */
 
-  receive_patient : function (blood_inventory, patient) {
-
+  receive_patient: function(blood_inventory, patient) {
+    switch (patient.blood_type) {
+      case "AB_POS":
+        console.log("AB_POS inventory: ", blood_inventory);
+        if (blood_inventory.AB_POS >= 1) {
+          return BloodType.AB_POS;
+        } else if (blood_inventory.AB_NEG >= 1) {
+          return BloodType.AB_NEG;
+        } else if (blood_inventory.A_POS >= 1) {
+          return BloodType.A_POS;
+        } else if (blood_inventory.A_NEG >= 1) {
+          return BloodType.A_NEG;
+        } else if (blood_inventory.B_POS >= 1) {
+          return BloodType.B_POS;
+        } else if (blood_inventory.B_NEG >= 1) {
+          return BloodType.B_NEG;
+        } else if (blood_inventory.O_POS >= 1) {
+          return BloodType.O_POS;
+        } else if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else {
+          return false;
+        }
+        break;
+      case "AB_NEG":
+        console.log("AB_NEG inventory: ", blood_inventory);
+        if (blood_inventory.AB_NEG >= 1) {
+          return BloodType.AB_NEG;
+        } else if (blood_inventory.A_NEG >= 1) {
+          return BloodType.A_NEG;
+        } else if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else {
+          return false;
+        }
+        break;
+      case "A_POS":
+        console.log("A_POS inventory: ", blood_inventory);
+        if (blood_inventory.A_POS >= 1) {
+          return BloodType.A_POS;
+        } else if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else if (blood_inventory.O_POS >= 1) {
+          return BloodType.O_POS;
+        } else {
+          return false;
+        }
+        break;
+      case "A_NEG":
+        console.log("A_NEG inventory: ", blood_inventory);
+        if (blood_inventory.A_NEG >= 1) {
+          return BloodType.A_NEG;
+        } else if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else {
+          return false;
+        }
+        break;
+      case "B_POS":
+        console.log("B_POS inventory: ", blood_inventory);
+        if (blood_inventory.B_POS >= 1) {
+          return BloodType.B_POS;
+        } else if (blood_inventory.B_NEG >= 1) {
+          return BloodType.B_NEG;
+        } else if (blood_inventory.O_POS >= 1) {
+          return BloodType.O_POS;
+        } else if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else {
+          return false;
+        }
+        break;
+      case "B_NEG":
+        console.log("B_NEG inventory: ", blood_inventory);
+        if (blood_inventory.B_NEG >= 1) {
+          return BloodType.B_NEG;
+        } else if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else {
+          return false;
+        }
+        break;
+      case "O_POS":
+        console.log("O_POS inventory: ", blood_inventory);
+        if (blood_inventory.O_POS >= 1) {
+          return BloodType.O_POS;
+        } else if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else {
+          return false;
+        }
+        break;
+      case "O_NEG":
+        console.log("O_NEG inventory: ", blood_inventory);
+        if (blood_inventory.O_NEG >= 1) {
+          return BloodType.O_NEG;
+        } else {
+          return false;
+        }
+    }
     // give a random blood type to anyone
     // very bad idea!
-    return [
-      BloodType.AB_POS,
-      BloodType.AB_NEG,
-      BloodType.A_POS,
-      BloodType.A_NEG
-    ][Math.floor(Math.random()*4)];
-
+    // return [
+    //   BloodType.AB_POS,
+    //   BloodType.AB_NEG,
+    //   BloodType.A_POS,
+    //   BloodType.A_NEG
+    // ][Math.floor(Math.random() * 4)];
   }
-
 };
